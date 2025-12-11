4d1e9c0fcf9f34d176c4dfa1fc1844262f36d66a
benpicard <ben.picard@web.de>
2025-12-11 22:48:14 +0100

    Cuda Native Tricky implementation

--- lettuce/ext/_boundary/wallfunction.py
+++ lettuce/ext/_boundary/wallfunction.py
@@ -119,6 +119,8 @@ def compute_wall_quantities(flow, dy, is_top: bool, acceleration = 0, newton_spe
 
         # Alte utau-Werte an diesen Stellen ersetzen
         utau[loglaw_mask] = utau_log
+        max_it=1
+        mean_it=1
 
     # yplus entsprechend neu berechnen
     yplus = dy * utau / viscosity
